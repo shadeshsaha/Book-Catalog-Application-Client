e6abe0b346e44193b6a2d21e2351678d76cdc1a5
shadeshsaha <shadesh15-8599@diu.edu.bd>
2023-07-18 18:24:04 +0600

    Resolved Issues and Updated Routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import AddNewBook from "../pages/Book/AddNewBook";
+import BookDetails from "../pages/Book/BookDetails";
 import BookPage from "../pages/Book/BookPage";
+import EditBookPage from "../pages/Book/EditBookPage";
 import Home from "../pages/Home/Home";
+import SignUp from "../pages/SignUp/SignUp";
 import NotFound from "../pages/shared/NotFound";
+import Login from "./../pages/Login/Login";
+import PrivateRoute from "./PrivateRoute";
 
 const routes = createBrowserRouter([
   {
@@ -16,6 +22,35 @@ const routes = createBrowserRouter([
       {
         path: "/books",
         element: <BookPage />,
+      },
+      {
+        path: "signup",
+        element: <SignUp />,
+      },
+      {
+        path: "add-new-book",
+        element: (
+          <PrivateRoute>
+            {" "}
+            <AddNewBook />{" "}
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "edit-book/:id",
+        element: (
+          <PrivateRoute>
+            <EditBookPage />
+          </PrivateRoute>
+        ),
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "/book-details/:id",
+        element: <BookDetails />,
       },
     ],
   },
